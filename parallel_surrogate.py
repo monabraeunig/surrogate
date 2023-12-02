bff337ce8064afaf2dce052fa93ec56d66e4e7cc
monabraeunig <138789374+monabraeunig@users.noreply.github.com>
2023-12-02 16:40:06 +0100

    Update parallel_surrogate.py
    overall improvements, train_gp still unclear

--- parallel_surrogate.py
+++ parallel_surrogate.py
@@ -22,21 +22,16 @@ class Surrogate(umbridge.Model):
         ## gaussian process
         self.gp = None
         ## Input data
-        self.in_list = None
-        ## gathered data
+        self.in_list = torch.empty((0, 2), dtype=torch.double)
+        ## Output data
+        self.out_list = torch.empty((0, 1), dtype=torch.double)
+        ## gathered data for updates
         self.in_queue = Queue()
         self.out_queue = Queue()
         ## Lock
         self.lock = threading.Lock()
-        self.lock2 = threading.Lock()
-        ##new attempt
-        ##self.gp wurde geupdatet (posterior noch nicht berechnet)
-        self.update_event = threading.Event()
-        #neue daten wurden in die in und ou Queue gelegt
+        ## new data available => gp should be updated
         self.update_data = threading.Event()
-        self.update_done = threading.Event()
-        self.update_done.set()
-        self.count = 0
         
     def get_input_sizes(self, config):
         return[2]
@@ -46,76 +41,65 @@ class Surrogate(umbridge.Model):
 
     ## gp gets trained
     #@profile
-    #@log_sparse(stack_depth=1)
     @log_sparse
     def train_gp(self, config):
-        with get_tracer().log_event("lock_in_use"):
-            with self.lock:
+        with self.lock:
+                
+            self.in_list = torch.cat([self.in_list] + list(self.in_queue.queue), dim=0)
+            self.in_queue = Queue()
+            self.out_list = torch.cat([self.out_list]+ list(self.out_queue.queue), dim=0)
+            self.out_queue = Queue()
             
-                if self.gp is not None:
-                    self.in_list = torch.cat([self.in_list] + list(self.in_queue.queue), dim=0)
-                    self.in_queue = Queue()
-                    self.out_list = torch.cat([self.out_list]+ list(self.out_queue.queue), dim=0)
-                    self.out_queue = Queue()
-
         new_gp = botorch.models.FixedNoiseGP(self.in_list, self.out_list, 1e-6*torch.ones_like(self.out_list), outcome_transform=Standardize(self.out_list.shape[1]), input_transform=Normalize(self.in_list.shape[1]))
 
-        infoin = torch.vstack([torch.tensor(self.in_list[-1]).flatten()], out=None)
-        with torch.no_grad():
-            posterior_ = new_gp.posterior(infoin)
+        #infoin = torch.vstack([torch.tensor(self.in_list[-1]).flatten()], out=None)
+        #with get_tracer().log_event("new_posterior"):
+            #with torch.no_grad():
+                #posterior_ = new_gp.posterior(infoin)
             
-        with get_tracer().log_event("lock_in_use"):    
+        with get_tracer().log_event("gp_übergabe"):
             with self.lock:
                 self.gp = new_gp
-                self.update_event.set()
-        
-    #@profile 
-    #@log_sparse(stack_depth=1)
-    @log_sparse
-    def __call__(self, parameters, config):
-        if self.gp is None:
+            
+            #self.gp = botorch.models.FixedNoiseGP(self.in_list, self.out_list, 1e-6*torch.ones_like(self.out_list), outcome_transform=Standardize(self.out_list.shape[1]), input_transform=Normalize(self.in_list.shape[1]))
+
+        #infoin = torch.vstack([torch.tensor(self.in_list[-1]).flatten()], out=None)
+        #with torch.no_grad():
+            #posterior_ = self.gp.posterior(infoin)
+        #self.update_event.set()
+    
+    @log_sparse    
+    def generate_new_data(self, parameters, config):
+        with get_tracer().log_event("model_output"):
             ##model_output
             out = self.umbridge_model(parameters)[0]
-            with get_tracer().log_event("lock_in_use"):
-                with self.lock:
-                    self.in_list = torch.tensor(parameters, dtype=torch.double)
-                    self.out_list = torch.tensor([out], dtype=torch.double)
-            self.train_gp(config)
-            
+        with self.lock:
+            self.in_queue.put(torch.tensor(parameters, dtype=torch.double))
+            self.out_queue.put(torch.tensor([out], dtype=torch.double))
+            self.update_data.set()
+        return out
+    
+    #@profile 
+    @log_sparse
+    def __call__(self, parameters, config):
+        
+        if self.gp is None:
+            out = self.generate_new_data(parameters, config)
             return [out]
         
         else:
             ## parameters to put into gp
             infoin = torch.vstack([torch.tensor(param).flatten() for param in parameters], out=None)
             ## let gp predict the output
-            ###
             with torch.no_grad():
-                time.sleep(0.1)
                 posterior_ = self.gp.posterior(infoin)
                 pos_variance = posterior_.variance
-            
-            if torch.any(torch.isnan(pos_variance)):
-                out = self.umbridge_model(parameters)[0]
-                with get_tracer().log_event("lock_in_use"):
-                    with self.lock:
-                        self.in_queue.put(torch.tensor(parameters, dtype=torch.double))
-                        self.out_queue.put(torch.tensor([out], dtype=torch.double))
-                        self.update_data.set()
-                        
-                return [out]
-            else:
-                    ## if variance is too high model is called
-                if torch.max(pos_variance) > 0.01:
-                    model_output = self.umbridge_model(parameters)[0]
-                    
-                    with get_tracer().log_event("lock_in_use"):
-                        with self.lock:
-                            self.in_queue.put(torch.tensor(parameters, dtype=torch.double))
-                            self.out_queue.put(torch.tensor([model_output], dtype=torch.double))
-                            self.update_data.set()
-                        
-                    return [model_output]
                 
+            ## if variance is too high model is called
+            if torch.max(pos_variance) > 0.0001:
+                model_output = self.generate_new_data(parameters, config)
+                return[model_output]
+                    
             pos_mean = posterior_.mean
             print("kein model/train_gp")
             return [pos_mean.flatten().tolist()]
@@ -123,28 +107,21 @@ class Surrogate(umbridge.Model):
     
     def supports_evaluate(self):
         return True
-        
     #@profile
-    #@log_sparse(stack_depth=2)
     def update_gp_thread(self):
         while True:
-            #time.sleep(1)
-            self.update_event.wait()
-            self.update_event.clear()
             self.update_data.wait()
             self.update_data.clear()
-            if not self.in_queue.empty() and not self.out_queue.empty():
-                print(self.out_queue.qsize())
-                if print(self.out_queue.qsize()) == 1:
-                    print(f'inlist: {list(self.out_queue.queue)}')
-                self.train_gp(None)   
-            #else: 
-                #self.update_event.set()
+            if self.gp is None:
+                ## achtung das geht nur wenn output size == 1 
+                if not self.out_queue.qsize() < 3:
+                    print(self.out_queue.qsize())
+                    self.train_gp(None)
+            else:
+                if not self.in_queue.empty() and not self.out_queue.empty():
+                    print(self.out_queue.qsize())
+                    self.train_gp(None) 
            
-                
-            
-    
-        
 testmodel = Surrogate()
 
 update_thread = threading.Thread(target=testmodel.update_gp_thread, daemon=True)
