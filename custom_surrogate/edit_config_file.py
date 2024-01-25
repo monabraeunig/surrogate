c13ef771f6d9c3bb1268967f9a23b325265d6468
monabraeunig <138789374+monabraeunig@users.noreply.github.com>
2024-01-25 11:34:00 +0100

    Update edit_config_file.py

--- custom_surrogate/edit_config_file.py
+++ custom_surrogate/edit_config_file.py
@@ -5,17 +5,17 @@ with open("custom_surrogate.json", "r") as f:
     data = json.load(f)
 
 ## Shape: torch.Size([output size, 1, input size])
-data["lengthscale"] =[[[0., 0.]]]
+data["lengthscale"] =[[[0.1151, 0.2556]]]
 with open(json_file_path, "w") as f:
     json.dump(data, f, indent=2)
 
 ## Shape: torch.Size([output size])
-data["outputscale"] = [0.]
+data["outputscale"] = [9.9763]
 with open(json_file_path, "w") as f:
     json.dump(data, f, indent=2)
 
 ## Shape: torch.Size([output size])
-data["mean"] = [0.]
+data["mean"] = [-6.7727]
 with open(json_file_path, "w") as f:
     json.dump(data, f, indent=2)
 
