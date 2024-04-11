0b285769b55ef60fb71c532bbcb1302a92c89ccb
monabraeunig <138789374+monabraeunig@users.noreply.github.com>
2024-04-11 10:01:21 +0200

    Update set_custom_configurations.py
    Added custom_hyperparameters

--- set_custom_configurations.py
+++ set_custom_configurations.py
@@ -10,24 +10,33 @@ data["model_port"] = 'http://0.0.0.0:4243'
 
 data["model_name"] = "posterior"
 
-## level of certainty gp must hold to accept prediction
+# Highest level of certainty to accept a prediction
 data["threshold"] = 0.0001
 
-## Shape: [output size, 1, input size]
-## except if :
-## input size == 1 and ouput size == 1 => [1,1]
-## input size == 2 and ouput size == 1 => [1,2]
+# True: Hyperparameters must be set manually below
+# False: Surrogate does hyperparameter optimization
+data["custom_hyperparameters"] = False
+
+
+## Hyperparameter
+# Shape: [output size, 1, input size]
+# except if :
+# input size == 1 and output size == 1 => [1,1]
+# input size == 2 and output size == 1 => [1,2]
 data["lengthscale"] =[[0, 0]]
 
-## Shape: [output size]
+# Shape: [output size]
 data["outputscale"] = [0]
 
-## Shape: [output size]
+# Shape: [output size]
 data["mean"] = [0]
 
-## set to True if variance plot is desired
+
+# Set to True if variance plot is desired
+# Only works for models with input size 2 and output size 1
 data["plot"] = False
-## set lower and upper bounds for the x any y axis in the plot
+
+# Set lower and upper bounds for the x (first input) and y (second input) axis in the plot
 data["lower_bound_x"] = -6
 data["lower_bound_y"] = -6
 data["upper_bound_x"] = 6
